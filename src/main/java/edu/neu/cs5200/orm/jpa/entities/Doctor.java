346a491fb23f0517c9bcfea3e0fbf204b3e37a5a
sumitb1606 <sumitbhanwala@Sumits-MacBook-Pro.local>
2018-08-05 17:44:08 -0400

    Added relationship between doctor and speciality

--- src/main/java/edu/neu/cs5200/orm/jpa/entities/Doctor.java
+++ src/main/java/edu/neu/cs5200/orm/jpa/entities/Doctor.java
@@ -3,26 +3,49 @@
  */
 package edu.neu.cs5200.orm.jpa.entities;
 
-import javax.persistence.Entity;
+import java.util.List;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.*;
+import javax.transaction.Transactional;
+
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+
+import org.springframework.beans.factory.annotation.Autowired;
 
 /**
  * @author sumitbhanwala
  *
  */
 @Entity
+@Table(name = "Doctor")
 public class Doctor extends Person {
 
 	/**
 	 * 
 	 */
+	@Column(name = "title")
 	private String title;
+	
+	@Column(name = "bio")
 	private String bio;
 	
-	
-	public Doctor() {
-		// TODO Auto-generated constructor stub
+
+	@ManyToMany
+	@JoinTable(name = "doctor_specialty",joinColumns = @JoinColumn(name = "doc_id", referencedColumnName = "id"), inverseJoinColumns = @JoinColumn(name = "specialty_id", referencedColumnName = "id"))
+	private List<Specialty> docspecialties;
+
+	public List<Specialty> getSpecialties() {
+		return docspecialties;
 	}
 
+	public void setSpecialties(List<Specialty> docspecialties) {
+		this.docspecialties = docspecialties;
+	}
 
 	public String getTitle() {
 		return title;
