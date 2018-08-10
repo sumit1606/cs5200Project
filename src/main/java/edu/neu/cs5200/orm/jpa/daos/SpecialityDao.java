e0002dda27308dd2875e7915310f9340a82c5160
sumitb1606 <sumitbhanwala@Sumits-MacBook-Pro.local>
2018-08-09 20:45:33 -0400

    Specialty dao

---
+++ src/main/java/edu/neu/cs5200/orm/jpa/daos/SpecialityDao.java
@@ -0,0 +1,50 @@
+/**
+ * 
+ */
+package edu.neu.cs5200.orm.jpa.daos;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import edu.neu.cs5200.orm.jpa.entities.Doctor;
+import edu.neu.cs5200.orm.jpa.entities.Specialty;
+import edu.neu.cs5200.orm.jpa.repositories.DoctorRepository;
+import edu.neu.cs5200.orm.jpa.repositories.SpecialtyRepository;
+
+/**
+ * @author sumitbhanwala
+ *
+ */
+public class SpecialityDao {
+	@Autowired
+	SpecialtyRepository specialtyRepository;
+	
+	public Specialty findSpecialtybyId(int id) {
+		if(specialtyRepository.findById(id) != null)
+			return specialtyRepository.findById(id).get();
+		else
+			return null;
+	}
+	
+	public Specialty findSpecialtyByName(String specialtyName) {
+		return specialtyRepository.findSpecialtyByName(specialtyName);	
+	}
+	
+	public void deleteSpecialtyById(int id) {
+		specialtyRepository.deleteById(id);
+	}
+	
+	public Specialty createSpecialty(Specialty s) {
+		if(this.findSpecialtyByName(s.getSpecialtyName()) != null) {
+			return specialtyRepository.save(s);
+		} else {
+			return this.findSpecialtyByName(s.getSpecialtyName());
+		}
+	}
+	
+	
+	
+	
+	
+	
+	
+}
