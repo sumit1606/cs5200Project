574d235ef63636226904555a9716e57dfa4d2707
Aashish Vikram Singh <aashishvikramsingh@Aashishs-MacBook-Pro.local>
2018-08-11 03:34:32 -0400

    removed test case call from main

--- src/main/java/edu/neu/cs5200/Cs5200DoctorManagementApplication.java
+++ src/main/java/edu/neu/cs5200/Cs5200DoctorManagementApplication.java
@@ -1,18 +1,14 @@
 package edu.neu.cs5200;
 
 
-import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import edu.neu.cs5200.orm.jpa.daos.HealthPersonnelDao;
-
 @SpringBootApplication
 public class Cs5200DoctorManagementApplication implements CommandLineRunner {
 
-	@Autowired
-	HealthPersonnelDao hpr;
 	
 	public static void main(String[] args) {
 
@@ -24,6 +20,6 @@ public class Cs5200DoctorManagementApplication implements CommandLineRunner {
 	
 	@Override
 	public void run(String... args) throws Exception {
-		hpr.test();
+
 	}
 }
